5311fe79d220d10e3b8997fb257ed1d2f7be7c4a
sungurko <sungurko@mail.ru>
2022-10-29 21:55:29 +0300

    #3_commit

--- task_1.py
+++ task_1.py
@@ -4,37 +4,35 @@ n = input('Введите число n: ')
 def summa_nn(n):
 	nn = int(n + n)
 	nnn = int(n + n + n)
-	res = int(n) + nn + nnn
-	print('результатом n + nn + nnn будет число:', res)
+	result = int(n) + nn + nnn
+	print(f'Результатом {n} + {nn} + {nnn} будет число:', result)
 
 # 2 Сложите цифры целого числа
 
 def summa_n(n):
 	n = int(n)
-	summ = 0
+	result = 0
 	while n > 0:
 		last = n % 10
 		summ += last
 		n = n // 10
-	print(summ)
+	print(result)
 
 # 2 Сложите цифры целого числа (второй вариант)
 
 def summa_n2(n):
 	result = sum(map(int, str(n)))
-	print(result)
+	print(f'Сумма цифр целого числа {n} равна: ',result)
 
-
-# Найти корень третьей степени числа 343
+# 3 Найти корень третьей степени числа 343
 #round(343 **(1/3))
 
 # В строке удалить все буквы а и подсчитать количество удаленных символов
 # Написать произвольную анкету и вывести полученные данные
 
 
-
 if __name__ == '__main__':
 	print('######## Результаты')
-	#summa_nn(n)
+	summa_nn(n)
 	#summa_n(n)
-	summa_n2(n)
+	#summa_n2(n)
