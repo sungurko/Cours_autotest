55a1ca6d752807fd3c70b768d8016de8fa01e6ed
sungurko <sungurko@mail.ru>
2022-11-04 23:16:33 +0300

    #8_commit

--- task_1_1.py
+++ task_1_1.py
@@ -27,20 +27,16 @@ def file_name(text: str) -> str:
 	print(result)
 
 # 8 Проверить что номера телефонов состоят только из цифр. Они могут начинаться с "+", цифры могут быть разделены пробелами и дефисами "-"
-#   Например, 8-999-777-1111, +7 999 333 2222, +7 999-555-11-11 
+#   Например, 8-999-777-1111, +7 999 333 2222, +7 999-555-11-11
 
-
-
-
-
-
-
-
-
-
-
-
-
+def number_phone(text):
+	result=[i for i in text if i !=' ' and i !='-']	
+	result = ''.join(result).lstrip('+')
+	if result.isnumeric():
+		print(f'Строка {text} состоит только из цифр !!!')
+	else:
+		print(f'В строке {text} есть буквы !!!')
+	
 
 
 
@@ -48,5 +44,6 @@ def file_name(text: str) -> str:
 if __name__ == '__main__':
 	print('######## Результаты ########')
 	#count_symbol(text)
-	file_name(text)
+	#file_name(text)
 	#delet_simvol()
+	number_phone(text)
